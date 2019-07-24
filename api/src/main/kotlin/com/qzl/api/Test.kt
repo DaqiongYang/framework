bad177f3d6b56a0f54ddf06088f4f2d266b40ced
18309315291 <2538096489@qq.com>
2019-07-24 20:52:57 +0800

    集成了接口文档swagger

--- api/src/main/kotlin/com/qzl/api/Test.kt
+++ api/src/main/kotlin/com/qzl/api/Test.kt
@@ -1,9 +1,11 @@
 package com.qzl.api
 
 import com.qzl.common.model.response.QueryResponseResult
-//import io.swagger.annotations.Api
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 
-//@Api(value = "测试接口", description = "框架搭建测试接口")
+@Api(value = "测试接口", description = "框架搭建测试接口")
 interface TestApi {
+    @ApiOperation(value = "测试方法")
     fun test(name:String): QueryResponseResult
 }
--- app/src/main/kotlin/com/qzl/app/AppApplication.kt
+++ app/src/main/kotlin/com/qzl/app/AppApplication.kt
@@ -1,8 +1,10 @@
 package com.qzl.app
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("com.qzl")//根据自己需要填写包名
 class AppApplication
 
 fun main(args: Array<String>) {
