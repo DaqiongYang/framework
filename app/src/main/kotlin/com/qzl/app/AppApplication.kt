b688f889d7d1d2d5ef9b97df9b284ccfd3ec8ace
18309315291 <2538096489@qq.com>
2019-08-14 23:47:03 +0800

    强周亮：添加微服务配置

--- app/src/main/kotlin/com/qzl/app/AppApplication.kt
+++ app/src/main/kotlin/com/qzl/app/AppApplication.kt
@@ -1,8 +1,10 @@
 package com.qzl.app
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.ComponentScan
 
+@EnableDiscoveryClient  //一个EurekaClient从EurekaServer发现服务
 @SpringBootApplication
 @ComponentScan("com.qzl")//根据自己需要填写包名
 class AppApplication
